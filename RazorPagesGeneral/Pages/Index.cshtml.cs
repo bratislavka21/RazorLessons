ec6b98b9e3363c6bb77977dfdb47ee0aa7988cc0
6955gagarinstreet@gmail.com <6955gagarinstreet@gmail.com>
2021-05-09 21:48:23 +0300

    improve index page

--- RazorPagesGeneral/Pages/Index.cshtml.cs
+++ RazorPagesGeneral/Pages/Index.cshtml.cs
@@ -12,6 +12,8 @@ namespace RazorPagesGeneral.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public string Message { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -19,7 +21,7 @@ namespace RazorPagesGeneral.Pages
 
         public void OnGet()
         {
-
+            Message = "Hello, blyat!";
         }
     }
 }
