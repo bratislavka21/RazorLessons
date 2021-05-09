43d15bedce7f228d7f8eefa6e4d90f5c06321f1b
6955gagarinstreet@gmail.com <6955gagarinstreet@gmail.com>
2021-05-09 14:13:10 +0300

    add employee model

---
+++ RazorPagesLessons.Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorPagesLessons.Models
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhotoPath { get; set; }
+
+        public Dept? Department { get; set; }
+    }
+}
